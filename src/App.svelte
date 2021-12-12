4d36784df3ccf140de97c39624f77934c679859d
Matt Moyes <M.Moyes@gmail.com>
2021-12-11 18:47:12 -0800

    added a navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,34 @@
 <script>
+	import Navbar from './navbar.svelte';
 	export let name;
 </script>
 
 <main>
+	<Navbar />
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<button class="button">Button</button> <button class="button is-dark">Dark</button>
+
+	<section class="section">
+		<h1 class="title">Section</h1>
+		<h2 class="subtitle">This is a section y'all</h2>
+		<p>blah blah blah</p>
+	</section>
+
+
+	<div class="columns">
+		<div class="column">
+			<textarea class="textarea is-dark" placeholder="Markdown Code"></textarea>
+		</div>
+		<div class="column">
+			<div class="box">
+				<h1>Section</h1>
+				<h2>This is a section y'all</h2>
+				<p>blah blah blah</p>				
+			</div>
+		</div>
+	</div>
+
 </main>
 
 <!-- <style>
@@ -33,4 +56,8 @@
 <style lang="text/scss">
 @import "../node_modules/bulma/sass/utilities/_all.sass";
 @import "./node_modules/bulma/sass/elements/button.sass";
+@import "./node_modules/bulma/sass/elements/box.sass";
+@import "./node_modules/bulma/sass/layout/section.sass";
+@import "./node_modules/bulma/sass/grid/columns.sass";
+@import "./node_modules/bulma/sass/form/_all.sass";
 </style>
---
+++ src/navbar.svelte
@@ -0,0 +1,20 @@
+<nav class="navbar is-warning" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <h1 class="navbar-item">📝🤓</h1>
+    </div>
+
+    <div class="navbar-end">
+        <div class="navbar-item">
+            <div class="buttons">
+                <a class="button is-danger"> Connect </a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+
+<style lang="text/scss">
+@import "../node_modules/bulma/sass/utilities/_all.sass";
+@import "../node_modules/bulma/sass/components/navbar.sass";
+@import "./node_modules/bulma/sass/elements/button.sass";
+</style>
